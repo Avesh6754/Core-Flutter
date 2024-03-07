3433dae76f63ea20e2b9aa9a891c6a5dfb9e6f7e
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-03-07 15:21:54 +0530

    Update qu_7.dart

--- Dart/Classroom 1/qu_7.dart
+++ Dart/Classroom 1/qu_7.dart
@@ -2,12 +2,12 @@ import'dart:io';
 void main()
 {
   print("Enter your First Name : ");
-  String x=String.parse(stdin.readLineSync()!);
+  String x=stdin.readLineSync()!;
   print("Enter Your Last Name  : ");
-  String y=String.parse(stdin.readLineSync()!);
-  String Full_Name;
-  Full_Name=x+y;
-  print(Full_Name);
+  String y=stdin.readLineSync()!;
+  String Full_Name=x+y;
+
+  print("$Full_Name ");
   
 
 
