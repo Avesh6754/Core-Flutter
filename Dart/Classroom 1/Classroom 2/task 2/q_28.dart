9dc137983adc2ffbe1c5b1112afb4eece0ef9d6b
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-03-14 13:12:31 +0530

    Update q_28.dart

--- Dart/Classroom 1/Classroom 2/task 2/q_28.dart
+++ Dart/Classroom 1/Classroom 2/task 2/q_28.dart
@@ -1,29 +1,15 @@
 
 
 import 'dart:io';
-
-
-class Unique
-{
-   dynamic Name;
-  
-  void set()
-  {
-   
-    stdout.write("Enter your Name : ");
-    Name=stdin.readLineSync()!;
- 
-    stdout.write("\nRemove all space : \n");
-    stdout.write("${Name.replaceAll(' ', '')}");
-
-  }
- 
-}
 void main()
 {
 
-  Unique u1=Unique();
-  
-    u1.set();
+ 
+    stdout.write("Enter your Name : ");
+    String Name=stdin.readLineSync()!;
+    
+    stdout.write("\nRemove all space : \n");
+    stdout.write("${Name.replaceAll(' ','')}");
+    
 
 }
