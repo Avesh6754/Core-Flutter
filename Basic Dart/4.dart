5abab3c8db0eb65dcef08e752bde536b79b51fbb
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-06-01 11:49:42 +0530

    Update 4.dart

--- Basic Dart/4.dart
+++ Basic Dart/4.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main() {
   stdout.write("Enter the value of n : ");
   int n = int.parse(stdin.readLineSync()!);
-  int sum = 0, i = -1, j = 1;
+  int sum = 0, i = 0, j = 1;
 
   while (i < n) {
     print(i);
