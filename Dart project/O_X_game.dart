ab26562259206b46b19c53afc906a0c1eaa4afad
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-06-04 16:17:22 +0530

    Update O_X_game.dart

--- Dart project/O_X_game.dart
+++ Dart project/O_X_game.dart
@@ -4,7 +4,11 @@ import 'Ox_Function.dart';
 
 void main()
 {
-  List l1=["-","-","-","-","-","-","-","-","-","-"];
-  int check=0;
+  List l1=["-","-","-","-","-","-","-","-","-"];
+  int check;
+  stdout.write("Choose your side Player (0 for X ) (1 for O) : ");
+  check=int.parse(stdin.readLineSync()!);
   Print(l1,check);
+
+  
 }
