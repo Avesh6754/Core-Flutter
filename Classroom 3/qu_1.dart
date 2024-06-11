b2bc9b925717850810c953131211bb01ffb13336
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-06-11 14:44:43 +0530

    Update qu_1.dart

--- Classroom 3/qu_1.dart
+++ Classroom 3/qu_1.dart
@@ -2,37 +2,41 @@ import 'dart:io';
 
 class Employee
 {
-  var age,salary,id;
-  String? name;
+  late int age,id;
+  late double salary;
+  late String name;
 
   void set(int i)
   {
-     stdout.write("Employee detail : $i");
-    stdout.write("Enter your id : ");
+     stdout.write("Employee detail : $i\n");
+    stdout.write("Enter your id : \n");
     id=int.parse(stdin.readLineSync()!);
-    stdout.write("Enter your name : ");
+    stdout.write("Enter your name : \n");
     name=stdin.readLineSync()!;
-    stdout.write("Enter your age : ");
+    stdout.write("Enter your age : \n");
     age=int.parse(stdin.readLineSync()!);
-    stdout.write("Enter your salary : ");
-    salary=int.parse(stdin.readLineSync()!);
+    stdout.write("Enter your salary : \n");
+    salary=double.parse(stdin.readLineSync()!);
 
   }
 
   void get(int i)
   {
     stdout.write("Employee detail below : $i");
-    Map<dynamic,String> ep = 
+    Map<String,dynamic> ep = 
     {
-        'Name':'name',
+        'Name':name,
         'id':id,
         'age':age,
         'salary':salary
     };
-    print(ep);
+    print("${ep['Name']}");
+    print("${ep['id']}");
+    print("${ep['age']}");
+    print("${ep['salary']}");
   }
 }
-void main
+void main()
 {
   stdout.write("Enter size : ");
   int n=int.parse(stdin.readLineSync()!);
