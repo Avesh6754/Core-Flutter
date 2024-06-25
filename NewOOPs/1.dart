9bcb7ec70a34fff218d1d934ef1707f943e7ca02
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-06-25 11:01:15 +0530

    Update 1.dart

--- NewOOPs/1.dart
+++ NewOOPs/1.dart
@@ -27,13 +27,13 @@ void main()
 {
   List <Employee> E1=[];
   Employee s1=Employee();
-  for(int i=0;i<3;i++)
+  for(int i=0;i<5;i++)
   {
       Employee s1=Employee();
       s1.Set();
       E1.add(s1);
   }
-  for(int i=0;i<3;i++)
+  for(int i=0;i<5;i++)
   {
     E1[i].Get();
   }
