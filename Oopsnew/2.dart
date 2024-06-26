07ca9a086ea5a4a00b531a1fa7f5d8dd49af5ca7
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-06-26 19:20:49 +0900

    Add files via upload

--- Oopsnew/2.dart
+++ Oopsnew/2.dart
@@ -2,9 +2,28 @@
 import 'dart:io';
 
 class Supermarket{
- late int _itemNumber,_n,_Quantity,_Tax;
+ late int _itemNumber,_n,_Quantity,_Tax,_check=0;
  late double _Discount;
- late String _item_name;
+ late String _item_name,_password,_username;
+        int Verify()
+        {
+          stdout.write("\nEnter the user name : ");
+          _username=stdin.readLineSync()!;
+          
+          stdout.write("\nEnter the Password : ");
+          _password=stdin.readLineSync()!;
+          if(_username=="Avesh" && _password=="8090")
+          {
+
+            return _check=1;
+            print("login Successfull: ");
+          }
+          else
+          {
+            
+                return _check=0;
+          }
+        }
 
 
         void Productinput()
@@ -33,7 +52,9 @@ class Supermarket{
         void Service()
         {
         
-          stdout.write("1 for All Product Records : \n");
+         stdout.write("1 for Search items by its item number  : \n");
+          stdout.write("2 for All Product Records : \n");
+
           stdout.write("0 for Exit : \n");
           stdout.write("Enter your choice : \n");
           _n=int.parse(stdin.readLineSync()!);
@@ -44,42 +65,64 @@ void main()
 {
   List <Supermarket> E1=[];
   Supermarket s1=Supermarket();
-  for(int i=0;i<2;i++)
+  Supermarket Temp;
+  
+  for(int i=0;i<3;i++)
   {
       Supermarket s1=Supermarket();
-      s1.set();
+      s1.Productinput();
       E1.add(s1);
   }
+  print("Username : Avesh");
+  print("Password : 8090");
+  int check=s1.Verify();
+  
 
- do
+ if(check==1)
  {
-
-    s1.choice();
-    switch(s1._n)
+  for(int i=0;i<E1.length;i++)
+  {
+     print("Here the item nmber for search : ${ E1[i]._itemNumber}");
+  }
+    do
     {
-        case 1:for(int i=0;i<E1.length;i++)
-        {
-            E1[i].get();
-        }
-        break;
-        
-        case 2:
-        stdout.write("Enter the train number : \n");
+      s1.Service();
+      switch(s1._n)
+      {
+        case 1:print("Enter the item number for its details : ");
         int x=int.parse(stdin.readLineSync()!);
         for(int i=0;i<E1.length;i++)
         {
-            if(x==E1[i]._TainNumber)
+            if(x==E1[i]._itemNumber)
             {
-              E1[i].get();
-              break;
+              E1[i].Productdeatils();
             }
-            
         }
-        
-        break;
-        case 0:print("you exit : ")
-       
-    }
+        case 2:print("Display of All deatils of items : ");
+        for(int i=0;i<E1.length;i++)
+        {
+            for(int j=i+1;j<E1.length;j++)
+            {
+                if(E1[i]._itemNumber!>E1[j]._itemNumber)
+                {
+                  Temp=E1[i];
+                  E1[i]=E1[j];
+                  E1[j]=Temp;
+                }
+            }
+        }
+        for(int j=0;j<E1.length;j++)
+        {
+          E1[j].Productdeatils();
+        }
+      }
+    
 
- }while(s1._n!=0);
+    }while(s1._n!=0);
+ }
+ else 
+ {
+   print("login not Successfull: ");
+    s1.Verify();
+ }
 }
