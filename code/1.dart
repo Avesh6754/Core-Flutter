cee3acb778e8c6fac9be95922e1891bcd39a03b3
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-07-17 14:09:56 +0530

    Update 1.dart

--- code/1.dart
+++ code/1.dart
@@ -7,36 +7,38 @@ void main() {
   List cust_card = [];
   List cust_deatils = [];
 
-  do {
+ do {
     c1.customerchoice();
     switch (c1.z) {
       case 1:
+        count = 1;
         print("Enter the customer ${number} Details : ");
         Customer c1 = Customer();
         c1.customerSignup();
         cust_deatils.add(c1);
         number++;
         do {
-          if (count != 0) {
+          if (count == 1) {
+            count--;
             c1.CustomerProductoption();
             if (c1.quantity != 0) {
               Map AddtoCard = {
                 'name': Product[c1.product - 1]['name'],
                 'id': c1.cust_id,
-                'pro_id': Product[c1.product - 1]['pro_id'],
+                // 'pro_id': Product[c1.product - 1]['pro_id'],
                 'quantity': c1.quantity,
                 'price': Product[c1.product - 1]['price']
               };
               cust_card.add(AddtoCard);
-            } else {
-              print("Thank you for visit : ");
-              break;
             }
-            stdout.write("Enter 1 for you want to add another product : ");
-            int n = int.parse(stdin.readLineSync()!);
-            count = n;
+          } else {
+            print("Thank you for visit : ");
+            break;
           }
-          c1.customerchoice();
+
+          stdout.write("Enter 1 for you want to add another product : ");
+          int n = int.parse(stdin.readLineSync()!);
+          count = n;
         } while (true);
         break;
       case 2:
@@ -50,7 +52,7 @@ void main() {
               if (cust_card[j]['id'] == cust_deatils[i].cust_id) {
                 print("Name : ${cust_card[j]['name']}");
                 print("id : ${cust_card[j]['id']}");
-                print("pro_id : ${cust_card[j]['pro_id']}");
+                // print("pro_id : ${cust_card[j]['pro_id']}");
                 print("Quantity : ${cust_card[j]['quantity']}");
                 print("Price : ${cust_card[j]['price']}");
               }
@@ -64,7 +66,7 @@ void main() {
         }
 
         break;
-      case 3:stdout.write("Customer details : ");
+      case 3:
         for (int i = 0; i < cust_deatils.length; i++) {
           cust_deatils[i].customeroutput();
           for (int j = 0; j < cust_card.length; j++) {
