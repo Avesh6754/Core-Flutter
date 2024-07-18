40a50f4c51425bc3245f6d32d86f0b058616914f
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-07-18 08:18:02 +0530

    Update globalfile.dart

--- code/globalfile.dart
+++ code/globalfile.dart
@@ -1,80 +1,86 @@
 import 'dart:io';
-class Customer {
-  late int cust_id;
-  late String cust_name;
-  late double cust_contact;
-  late int z;
-  late int product;
-  late int quantity;
-  late dynamic total = 0, discount = 0;
+class Customer{
+  late String cust_name, cust_contact;
+  late int cust_id,z, product, quantity ;
 
-  void customerSignup() {
-    stdout.write("\nEnter the customer Details : \n");
-    stdout.write("Enter the customer Id : ");
+  void customerinput() {
+ 
+    stdout.write("Enter the Customer ID: ");
     cust_id = int.parse(stdin.readLineSync()!);
-    stdout.write("Enter the customer Name : ");
+    stdout.write("Enter the Customer Name: ");
     cust_name = stdin.readLineSync()!;
-    stdout.write("Enter the customer Contact : ");
-    cust_contact = double.parse(stdin.readLineSync()!);
+    stdout.write("Enter the Customer Contact: ");
+    cust_contact = stdin.readLineSync()!;
+  }
+
+ void customeroutput(int i){
+    print("Here the ${i + 1} customer detail are Below.");
+    print("Customer ID     : $cust_id");
+    print("Customer Name   : $cust_name");
+    print("Customer Contact: $cust_contact");
   }
 
   void customerchoice() {
-    stdout.write("Enter 1 for Add number of customer  : \n");
-    stdout.write("Enter 2 for find the customer through id   : \n");
-    stdout.write("Enter 3 for display all customer  deatils: \n");
-    stdout.write("Enter 4 for Exit : \n");
-    stdout.write("\nEnter your choice : \n");
+    print("Enter 1 : Add the number of Customer");
+    print("Enter 2: find the customer details through Customer By ID");
+    print("Enter 3: Display all Customer");
+    print("Enter 4 : For Exit");
+    stdout.write(">> Enter Your Choice : ");
     z = int.parse(stdin.readLineSync()!);
+   
   }
 
-  void getter(int i) {
-    print("Customer details ${i + 1}");
-    print("Customer Id : ${cust_id}");
-    print("Customer Name : ${cust_name}");
-    print("Customer Contact : ${cust_contact}");
-  }
-
-  void CustomerProductoption(List Product) {
-    print("Enter 1 for  ${Product[0]['name']},${Product[0]['price']}:");
-    print("Enter 2 for  ${Product[1]['name']},${Product[1]['price']}:");
-    print("Enter 3 for  ${Product[2]['name']},${Product[2]['price']}:");
-    print("Enter 4 for  ${Product[3]['name']},${Product[3]['price']}:");
-    print("Enter 5 for  ${Product[4]['name']},${Product[4]['price']}:");
-    print("Enter 6 for  ${Product[5]['name']},${Product[5]['price']}:");
+  void customerProductoption() {
+    print("Enter 1 for  ${productlist[0]['name']} ,Price ${productlist[0]['price']}:");
+    print("Enter 2 for  ${productlist[1]['name']} ,Price ${productlist[1]['price']}:");
+    print("Enter 3 for  ${productlist[2]['name']} ,Price ${productlist[2]['price']}:");
+    print("Enter 4 for  ${productlist[3]['name']} ,Price ${productlist[3]['price']}:");
+    print("Enter 5 for  ${productlist[4]['name']} ,Price ${productlist[4]['price']}:");
+    print("Enter 6 for  ${productlist[5]['name']} ,Price ${productlist[5]['price']}:");
     print("Enter your choice : ");
     product = int.parse(stdin.readLineSync()!);
     if (product >= 1 && product <= 6) {
-      print("Enter the quantity : ");
+      stdout.write("Enter Quantity : ");
       quantity = int.parse(stdin.readLineSync()!);
     } else {
-      print("\n");
-      print("Please Enter valid input : ");
-      CustomerProductoption(Product);
+      print("\nPlease enter valid input between 1 to 6\n");
+      customerProductoption();
     }
   }
 
-  void Billamount(int i, List Product, List cust_deatils, List cust_card) {
-    for (int j = 0; j < cust_card.length; j++) {
-      if (cust_card[j]['id'] == cust_deatils[i].cust_id) {
-        total += (cust_card[j]['price'] * cust_card[j]['quantity']);
+  void billSystem(int i, List custcart, List custdetail, List productlist) {
+    dynamic  total = 0, Discount = 0;
+    for (int j = 0; j < custcart.length ; j++) {
+      if (custcart[j]['id'] == custdetail[i].cust_id) {
+        int temp1 = custcart[j]['price'];
+        int temp2 = custcart[j]['quantity'];
+        total += (temp1 * temp2);
       }
     }
+    late double d = 0;
 
-    print("Total amount : $total  ");
+    print("Total Amount :  $total");
+
     if (total >= 500 && total <= 1500) {
-      discount = 10;
-      total -= (total * discount) / 100;
+      d = 10;
+      Discount = (total * 10) / 100;
+      total -= Discount;
     } else if (total > 1500 && total <= 3500) {
-      discount = 20;
-      total -= (total * discount) / 100;
+      d = 20;
+      Discount = (total * 20) / 100;
+      total -= Discount;
     } else if (total > 3500 && total <= 6500) {
-      discount = 25;
-      total -= (total * discount) / 100;
+      d = 25;
+      Discount = (total * 25) / 100;
+      total -= Discount;
     } else if (total > 6500) {
-      discount = 30;
-      total -= (total * discount) / 100;
+      d = 30;
+      Discount = (total * 30) / 100;
+      total -= Discount;
     }
-    print("Discoount : %$discount ");
-    print("Final amoount $total ");
+    print("Percentage Discount : $d% ");
+    print("Discounts Amount : $Discount ");
+   
+    print("Payment: $total");
   }
 }
