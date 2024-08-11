54fd4cd670a0a95df4d01b3361cd88429cb965eb
Avesh kumar prajapati <149478146+Avesh6754@users.noreply.github.com>
2024-08-11 21:37:28 +0530

    Update sumofarray.dart

--- code/sumofarray.dart
+++ code/sumofarray.dart
@@ -5,8 +5,7 @@
 // When each customer is entered (instantiated) note their cust_id, cust_name, cust_contact
 // Create a menu driven system which gives the customer verities of categories and their products.
 // Whenever user selects any product simulate the feature to add product in cust_cart which will contain 4 parameters for each product pro_id, pro_name, pro_qty, pro_price.
-// Apply billing and discount criteria's on the final amount after calculating each customer's number of product's price.
-//                                  10% disc. on 500 - 1500
+// Apply billing and  disc. on 500 - 1500
 //                                  20% disc. on 1500 - 3500
 //                                  25% disc. on 3500 - 6500
 //                                  30% disc. for above all.
